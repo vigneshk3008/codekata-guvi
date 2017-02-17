372a999119fc12a3858419b79c2973bd384d1ac7
vigneshk3008 <vigneshcse007@gmail.com>
2017-02-17 12:36:09 +0530

    Create vk_vowel_cons.java

---
+++ vk_vowel_cons.java
@@ -0,0 +1,45 @@
+import java.util.*;
+public class HelloWorld{
+
+     public static void main(String []args){
+       String n;
+       Scanner a=new Scanner(System.in);
+       n = a.next();
+       switch(n)
+       {
+           case "a":
+               System.out.println("the given char is vowel");
+               break;
+           case "e":
+               System.out.println("the given char is vowel");
+               break;
+           case "i":
+               System.out.println("the given char is vowel");
+               break;
+           case "o":
+               System.out.println("the given char is vowel");
+               break;
+           case "u":
+               System.out.println("the given char is vowel");
+               break;
+           case "A":
+               System.out.println("the given char is vowel");
+               break;
+           case "E":
+               System.out.println("the given char is vowel");
+               break;
+           case "I":
+               System.out.println("the given char is vowel");
+               break;
+           case "O":
+               System.out.println("the given char is vowel");
+               break;
+           case "U":
+               System.out.println("the given char is vowel");
+               break;
+           default:
+               System.out.println("the given char is consonent");
+       }
+    
+     }
+}
