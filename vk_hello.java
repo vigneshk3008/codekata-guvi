4f74b89d5942c9d30f7d26075c5756b27ce64342
vigneshk3008 <vigneshcse007@gmail.com>
2017-02-17 14:34:55 +0530

    Create vk_hello.java

---
+++ vk_hello.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class HelloWorld{
+
+     public static void main(String []args)
+     {
+       for(int i=0;i<5;i++)
+       {
+           System.out.println("Hello");
+       }
+     }
+}
