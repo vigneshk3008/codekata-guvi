4ad58a9b9b79daf9b12bbbd70c561c1be203226c
vigneshk3008 <vigneshcse007@gmail.com>
2017-02-24 11:29:14 +0530

    Create vk_43.java

---
+++ vk_43.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.io.*;
+public class HelloWorld{
+
+     public static void main(String []args){
+        String s1="",s2="";
+        Scanner sc=new Scanner(System.in);
+        s1=sc.nextLine();
+        s2=sc.nextLine();
+        System.out.println("the concatination of two given strings are "+s1+" "+s2);
+     }
+}
