c5670973aa08383498062af46bd58f39ba20b14c
vigneshk3008 <vigneshcse007@gmail.com>
2017-02-24 12:00:15 +0530

    Create vk_44.java

---
+++ vk_44.java
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+    int i;
+    printf("Enter Your choise from 1 to 9");
+    scanf("%d",&i);
+    switch(i)
+    {
+        case 0:
+            {
+                printf("Your choise is 0");
+                break;
+            }
+        case 1:
+            {
+                printf("Your choise is 1");
+                break;
+            }
+        case 3:
+            {
+                printf("Your choise is 3");
+                break;
+            }
+        case 4:
+            {
+                printf("Your choise is 4");
+                break;
+            }
+        case 5:
+            {
+                printf("Your choise is 4");
+                break;
+            }
+        case 6:
+            {
+                printf("Your choise is 4");
+                break;
+            }
+        case 7:
+            {
+                printf("Your choise is 4");
+                break;
+            }
+        case 8:
+            {
+                printf("Your choise is 4");
+                break;
+            }
+        case 9:
+            {
+                printf("Your choise is 4");
+                break;
+            }
+        defalut :
+            {
+                printf("you entered number is not in range");
+                break;
+            }
+    }
+    return 0; 
+    
+}
+
